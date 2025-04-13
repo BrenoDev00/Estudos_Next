172285c2fc2174a6bb7c03c6b6a64178a8d25014
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-04-13 15:36:17 -0300

    ajusta responsividade

--- task-app/src/app/page.tsx
+++ task-app/src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
     <main
       className={twMerge(
         "bg-[#0F0F0F] w-screen h-screen",
-        "flex justify-center items-center flex-col"
+        "flex justify-center items-center flex-col p-4"
       )}
     >
       <div>
         <Image
-          className="max-w-[480px]"
+          className={twMerge("max-w-[480px]", "max-md:max-w-[300px]")}
           alt="Logo Tarefas+."
           src={heroImage}
           priority
@@ -22,11 +22,39 @@ export default function Home() {
       <h1
         className={twMerge(
           "text-white text-[38px] text-center",
-          "font-bold mt-[69px]"
+          "font-bold mt-[69px]",
+          "max-md:text-[28px] max-sm:text-[22px]"
         )}
       >
         Sistema feito para você organizar <br /> seus estudos e tarefas
       </h1>
+
+      <div
+        className={twMerge(
+          "flex items-center gap-[24px] mt-[58px]",
+          "max-sm:flex-col"
+        )}
+      >
+        <article
+          className={twMerge(
+            "font-bold text-[22px] text-[#0f0f0f] bg-[#fafafa] text-center w-[300px]",
+            "py-[14px] px-[44px] rounded-[8px] hover:scale-110 transition duration-300",
+            "max-md:text-[12px] max-sm:text-[10px] max-md:py-[10px] max-md:px-[30px]"
+          )}
+        >
+          + 7 mil posts
+        </article>
+
+        <article
+          className={twMerge(
+            "font-bold text-[22px] text-[#0f0f0f] bg-[#fafafa] text-center w-[300px]",
+            "py-[14px] px-[44px] rounded-[8px] hover:scale-110 transition duration-300",
+            "max-md:text-[12px] max-sm:text-[10px] max-md:py-[10px] max-md:px-[30px]"
+          )}
+        >
+          + 1 mil comentários
+        </article>
+      </div>
     </main>
   );
 }
