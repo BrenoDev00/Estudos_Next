a68d71d0d860f0d374817c9ae2caa6ca983dbb65
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-04-24 17:58:36 -0300

    adiciona pequenos ajustes nos componentes

--- task-app/src/app/dashboard/layout.tsx
+++ task-app/src/app/dashboard/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import "../globals.css";
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Página de dashboard.",
 };
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-br">
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
