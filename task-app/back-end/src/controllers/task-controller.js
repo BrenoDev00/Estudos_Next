42844e9a7bac850016b96a3c7a4bb3deca593778
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-05-22 17:51:38 -0300

    adiciona camada de controllers para tasks

---
+++ task-app/back-end/src/controllers/task-controller.js
@@ -0,0 +1,35 @@
+import { TaskRepository } from "../repositories/task-repository.js";
+import { taskSchema } from "../schemas/task-schema.js";
+import { taskColumnsToInsert } from "../utils/constants/table-columns.js";
+import snakeCaseKeys from "snakecase-keys";
+
+export class TaskController {
+  async getTasksByMostRecentDate(request, response) {
+    const result = await new TaskRepository().getTasksByMostRecentDate();
+
+    return response.status(200).send(result);
+  }
+
+  async createTask(request, response) {
+    const { body } = request;
+
+    const bodyValidation = taskSchema.safeParse(body);
+
+    if (!bodyValidation.success)
+      return response.status(400).send(bodyValidation.error.errors);
+
+    const formatedBody = snakeCaseKeys(body);
+
+    const values = taskColumnsToInsert.reduce((acc, columnName) => {
+      acc.push(formatedBody[columnName]);
+
+      return acc;
+    }, []);
+
+    await new TaskRepository().createTask(values);
+
+    return response
+      .status(201)
+      .send({ message: "Tarefa adicionada com sucesso!" });
+  }
+}
--- task-app/back-end/src/routes/task-routes.js
+++ task-app/back-end/src/routes/task-routes.js
@@ -1,34 +1,12 @@
-import { TaskRepository } from "../repositories/task-repository.js";
 import { Router } from "express";
-import { taskColumnsToInsert } from "../utils/constants/table-columns.js";
-import snakeCaseKeys from "snakecase-keys";
-import { taskSchema } from "../schemas/task-schema.js";
+import { TaskController } from "../controllers/task-controller.js";
 
 export const taskRouter = Router();
 
 taskRouter.get("/", async (request, response) => {
-  const result = await new TaskRepository().getTasksByMostRecentDate();
-
-  return response.status(200).send(result);
+  return await new TaskController().getTasksByMostRecentDate(request, response);
 });
 
 taskRouter.post("/", async (request, response) => {
-  const { body } = request;
-
-  const bodyValidation = taskSchema.safeParse(body);
-
-  if (!bodyValidation.success)
-    return response.status(400).send(bodyValidation.error.errors);
-
-  const formatedBody = snakeCaseKeys(body);
-
-  const values = taskColumnsToInsert.reduce((acc, columnName) => {
-    acc.push(formatedBody[columnName]);
-
-    return acc;
-  }, []);
-
-  await new TaskRepository().createTask(values);
-
-  return response.status(201).send({ message: "Tarefa criada com sucesso!" });
+  return await new TaskController().createTask(request, response);
 });
