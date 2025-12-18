212b9ce2fdbf71081878ebf16daf226cac06027b
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-12-18 15:56:06 -0300

    adiciona lógica de rota ativa no header

--- app-router/src/components/header.tsx
+++ app-router/src/components/header.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export const Header: React.FC = () => {
+  const pathName = usePathname();
+
+  const routesList: { name: string; href: string }[] = [
+    { name: "Home", href: "/" },
+    { name: "Contatos", href: "/contatos" },
+  ];
+
   return (
     <header className="bg-black text-white w-full py-2 px-4">
-      <h1>Header da aplicação</h1>
+      <nav>
+        <ul className="flex items-center gap-6">
+          {routesList.map((route) => (
+            <li key={route.name}>
+              <Link
+                href={route.href}
+                className={`cursor-pointer ${
+                  pathName === route.href && "text-purple-600"
+                }`}
+              >
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
