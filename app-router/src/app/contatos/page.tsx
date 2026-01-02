ee4d04ffcfd11178cfc6d36a33e6a22d7d595bc3
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-01-02 12:56:21 -0300

    adiciona componente de loading

--- app-router/src/app/contatos/page.tsx
+++ app-router/src/app/contatos/page.tsx
@@ -3,6 +3,7 @@
 import { DollarRateData } from "@/src/models/dollar-rate-data";
 import { useDollarRate } from "@/src/shared/hooks/use-dollar-rate";
 import { useState, useEffect } from "react";
+import Loading from "../loading";
 
 export default function Contacts() {
   const { getDollarRate } = useDollarRate();
@@ -21,14 +22,21 @@ export default function Contacts() {
 
   return (
     <>
-      <h1>Página de contatos</h1>;
-      <h2 className="mt-4 text-center text-xl">
-        {dollarRateData?.USDBRL.name}
-      </h2>
-      <div className="mt-2 flex flex-col gap-2 items-center">
-        <p>Alta: {dollarRateData?.USDBRL.high}</p>
-        <p>Mínima: {dollarRateData?.USDBRL.low}</p>
-      </div>
+      <h1>Página de contatos</h1>
+
+      {dollarRateData ? (
+        <>
+          <h2 className="mt-4 text-center text-xl">
+            {dollarRateData?.USDBRL.name}
+          </h2>
+          <div className="mt-2 flex flex-col gap-2 items-center">
+            <p>Alta: {dollarRateData?.USDBRL.high}</p>
+            <p>Mínima: {dollarRateData?.USDBRL.low}</p>
+          </div>
+        </>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 }
---
+++ app-router/src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="w-full bg-white flex items-center justify-center">
+      <p className="text-2xl text-black font-semibold text-center">
+        Carregando...
+      </p>
+    </div>
+  );
+}
